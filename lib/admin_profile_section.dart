0228c4e8aea978864143346c3d21f8e02d35b997
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-12-01 15:22:59 +0500

    admin avatar added

--- lib/admin_profile_section.dart
+++ lib/admin_profile_section.dart
@@ -6,10 +6,37 @@ class adminprofilesection extends StatelessWidget {
     return Container(
         margin: EdgeInsets.all(20),
         padding: EdgeInsets.all(16),
+        //structure of the container
         decoration: BoxDecoration(
           color: Color(0xFF2E7835),
           borderRadius: BorderRadius.circular(10),
-
+        ),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+               children: [
+                //Circle portion for profile picture
+                CircleAvatar(
+                   radius: 30,
+                   backgroundColor: Colors.white,
+                   child: Column(
+                    children:[ 
+                    Icon(
+                    Icons.person, size: 40,color: Colors.black,
+                   ),
+                   Text('Upload Picture',
+                   style: TextStyle(
+                     fontSize: 5,
+                   ),),
+                   
+                   ],
+                   ),
+                   
+                ),
+               ],
+            ),
+          ],
         ),
     );
   }
