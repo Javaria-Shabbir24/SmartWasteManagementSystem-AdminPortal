7348197ecb2409425c47c547a3911fd7ec9b6b88
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-12-14 17:54:50 +0500

    Stats Cards

--- lib/admin_manage_staff.dart
+++ lib/admin_manage_staff.dart
@@ -5,7 +5,7 @@ class AdminManageStaff extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar( 
         backgroundColor:const  Color(0xFFE6F3EC),
         title: const Text("Manage Staff",
         style: TextStyle(
@@ -35,16 +35,19 @@ class AdminManageStaff extends StatelessWidget {
               thickness: 1,
             ),
             Container(
-              margin: const EdgeInsets.all(18),
-              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(19),
+              padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
                 color: const Color(0xFF2E7835),
                 borderRadius: BorderRadius.circular(10),
               ),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  card(),
+                  //card showing the registered employees
+                  statCard(10,"  Employees  "),
+                  //card showing the pending hiring requests
+                  statCard(10,"Hire Requests"),
                 ],
               ),
             ),
@@ -59,6 +62,32 @@ class AdminManageStaff extends StatelessWidget {
 // returning a card showing the statistics
 Widget statCard(int count, String label){
   return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    color: const Color(0xFFB7EFBD),
+    child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        //printing the count
+        Text(count.toString(),
+        textAlign: TextAlign.left,
+        style: const TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color:  Color(0xFF2E7835),
+        ),
+        ),
+        const SizedBox(height: 10),
+        //printing the label
+        Text(label,
+        style: const TextStyle(
+          fontWeight: FontWeight.w600,
+        ),
+        ),
+      ],
+    ),
     
+    ),
   );
 }
