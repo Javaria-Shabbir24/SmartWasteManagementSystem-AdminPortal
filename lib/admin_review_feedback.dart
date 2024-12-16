075ce7ff13f7a886c6e8812e8995446e202f7db8
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-12-16 17:20:30 +0500

    review feedback appBar and Bottom

--- lib/admin_review_feedback.dart
+++ lib/admin_review_feedback.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'admin_bottom_section.dart';
 
-class AdminReviewFeedback extends StatelessWidget {
+class AdminReviewFeedback extends StatefulWidget {
   const AdminReviewFeedback({super.key});
 
   @override
+  State<AdminReviewFeedback> createState() => _AdminReviewFeedbackState();
+}
+
+class _AdminReviewFeedbackState extends State<AdminReviewFeedback> {
+  @override
   Widget build(BuildContext context) {
+    //list of feedback types
+    List<String> feedbackTypes=['Complaints','Suggestions','Questions'];
+    //variable to store the selected feedback type
+    String? selectedFeedbackType;
     return Scaffold(
       appBar: AppBar( 
         backgroundColor:const  Color(0xFFE6F3EC),
@@ -24,6 +33,83 @@ class AdminReviewFeedback extends StatelessWidget {
             )
         ],
       ),
+      backgroundColor: const Color(0xFFE6F3EC),
+      body: SingleChildScrollView(
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            
+            Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
+            Container(
+              padding: EdgeInsets.all(7),
+              margin: EdgeInsets.only(top: 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xFF2E7835),width: 3),
+              ),
+              child: Text('Employee feedback on waste management\nis available for your review.',
+              style: TextStyle(
+              fontSize: 16,
+            ),
+
+            )
+            
+            ),
+            const SizedBox(height: 15,),
+            Container(
+              padding: EdgeInsets.only(left: 14,right: 14,top: 5,bottom: 5),
+              
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //dropdown for selecting the feedback type
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Select Feedback Type:',
+                          style: TextStyle(
+                            color: const Color(0xFF2E7835),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 10,),
+                        Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              border: Border.all(color: Color(0xFF2E7835)),
+                            ),
+                            child: DropdownButton<String>(
+                            hint: Text(selectedFeedbackType ?? 'Select'),
+                            value: selectedFeedbackType,
+                            items: feedbackTypes.map((String type){
+                              return DropdownMenuItem(
+                                value: type,
+                                child: Text(type),
+                              );
+                            }).toList(),
+                            onChanged:(String? newvalue){
+                              setState(() {
+                                selectedFeedbackType=newvalue;
+                              });
+                              
+                            },
+                            
+                          ),
+                        ),
+                      ],
+                  ),
+                ],
+              )
+            )
+          ],
+        ),
+      ),
+      //bottom navigation bar
       bottomNavigationBar: AdminBottomSection(),
     );
   }
