f27df3484d68f78eb9bd043f643fed24ce05bd10
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-12-30 02:49:50 +0500

    manage leave request page in progress

--- lib/admin_manage_leave_requests.dart
+++ lib/admin_manage_leave_requests.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'admin_bottom_section.dart';
 
-class AdminManageLeaveRequests extends StatelessWidget {
+class AdminManageLeaveRequests extends StatefulWidget {
   const AdminManageLeaveRequests({super.key});
+
+  @override
+  State<AdminManageLeaveRequests> createState() => AdminManageLeaveRequestsState();
+}
+
+class AdminManageLeaveRequestsState extends State<AdminManageLeaveRequests> {
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +30,70 @@ class AdminManageLeaveRequests extends StatelessWidget {
             )
         ],
       ),
+      backgroundColor: const Color(0xFFE6F3EC),
+      body: SingleChildScrollView(
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            
+            const Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xFF2E7835),width: 9),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: const Text('Leaves pending approval are listed below for review and action',
+              style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            )
+            ),
+            const SizedBox(height: 15,),
+            Container(
+              padding: const EdgeInsets.only(left: 14,right: 14,top: 5,bottom: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                         
+                      ),
+                    ),
+                  )
+                ],
+              )
+            )
+          ],
+        ),
+      ),
+      //bottom navigation bar
       bottomNavigationBar: AdminBottomSection(),
     );
+  }
+}
+
+//backend code
+//defining a class for Employee details
+class Employee{
+  int employeeID=0;
+  int employeeName=0;
+  String requestType='';
+  String summary='';
+  String requestStatus='';// to maintain track of replied and unreplied leave requests
+  String response='';//the response entered by admin
+  //constructor
+  Employee(int iD,int capacity, int fillLevel, String assignedTo, String location, int time)
+  {
   }
 }
